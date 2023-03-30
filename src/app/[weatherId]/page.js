982f6755e07943e880a3bada548f1d02734bd466
Paulo Dionisio <devpaulodvo@gmail.com>
2023-03-30 19:01:29 +0800

    inital commit

--- src/app/[weatherId]/page.js
+++ src/app/[weatherId]/page.js
@@ -1,9 +1,19 @@
-import React from "react";
+'use client'
+import React, {use, useEffect} from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-export default function weatherPage() {
+export default function weatherPage({params: {value}}) {
+    const router = useRouter()
+    const {data: session} = useSession();
+
+    useEffect(()=>{
+        if(!session){
+            router.push('/login');
+        }
+    }, [])
     return(
         <div>
-            Weather
         </div>
     )
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './searchbar.module.css';
+import cities from '../../pages/api/cities/ph.json';
+import Link from "next/link";
+
 export default function SearchBar(){
+    const [value, setValue] = useState('');
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const onSearch = (searchItem, lat, lng) => {
+        setValue(searchItem);
+        setLat(lat);
+        setLng(lng);
+        console.log('search', searchItem, lat, lng);
+    }
+
+    const checkWeather = (value, lat, lng) => {
+        
+    }
+
     return(
-        <input className={`${styles.searchbar}`} placeholder="Search" name="seachcity" id="searchcity"/>
+        <div className={`${styles.container}`}>
+            <input className={`${styles.searchbar}`} type="text" value={value} onChange={onChange}/>
+            {cities.filter(item=>{
+                const searchItem = value.toLowerCase();
+                const city = item.city.toLowerCase();
+                const lat = item.lat;
+                const lng = item.lng;
+                return searchItem && city.startsWith(searchItem) && city !== searchItem;
+                }).map((item, index)=>(
+                    <div key={index} onClick={()=>{onSearch(item.city, item.lat, item.lng)}}>
+                        {item.city}
+                    </div>
+                ))
+            
+            }
+            <Link className={`${styles.searchbtn}`} onClick={()=>onSearch(value, lat, lng)} href={`/${value}`}>Search</Link>
+        </div>
     )
 }
